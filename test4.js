cafcadbfc76b25a0bdce2ef8ad6b348f30eacf5d
Kevin Zhong9 <gejedah@users.noreply.github.com>
2025-06-16 14:17:45 +0000

    missing number timesss

--- test4.js
+++ test4.js
@@ -9,6 +9,13 @@ const numbers = [9, 6, 4, 2, 3, 5, 7, 0, 1];
 
 function result(numbers) {
   // Your Code Here
+  numbers.sort((a, b) => a - b);
+  for (let index = 0; index < numbers.length; index++) {
+    if (numbers[index + 1] !== numbers[index] + 1) {
+      return numbers[index] + 1;
+    }
+  }
+  return numbers.length;
 }
 
 console.log(result(numbers));
