15b48a427453e2fb92972d0b3007ce7aa1e7c445
Kevin Zhong9 <gejedah@users.noreply.github.com>
2025-06-16 20:41:31 +0700

    Add files via upload
    first commit

---
+++ test2.js
@@ -0,0 +1,20 @@
+/**
+ * Direction:
+ * Remove duplicated data from array
+ * 
+ * Expected Result:
+ * [1, 2, 3, 4, 5]
+ */
+const data = [1, 4, 2, 3, 5, 3, 2, 4];
+
+function result(data) {
+  // Your Code Here
+  let dictionary = {};
+  data.forEach(element => {
+    if (dictionary.element) {
+      data.pull
+    }
+  });
+}
+
+console.log(result(data));
